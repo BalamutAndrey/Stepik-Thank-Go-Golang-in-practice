9364d0774aa691eb97b72267a4062ddac850a33a
Andrey Balamut <balamut.andrey@gmail.com>
2023-01-10 17:48:16 +0300

    Add 1.4.1. The filter for a collection -- readme

---
+++ 1.4.1. The filter for a collection/src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func filter(predicate func(int) bool, iterable []int) []int {
+	// отфильтруйте `iterable` с помощью `predicate`
+	// и верните отфильтрованный срез
+}
+
+func main() {
+	src := readInput()
+	// отфильтруйте `src` так, чтобы остались только четные числа
+	// и запишите результат в `res`
+	// res := filter(...)
+	fmt.Println(res)
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// readInput считывает целые числа из `os.Stdin`
+// и возвращает в виде среза
+// разделителем чисел считается пробел
+func readInput() []int {
+	var nums []int
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		nums = append(nums, num)
+	}
+	return nums
+}
