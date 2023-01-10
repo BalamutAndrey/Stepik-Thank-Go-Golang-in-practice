2b9df8b2739e24a26116e04a59360e9155a726c7
Andrey Balamut <balamut.andrey@gmail.com>
2023-01-10 16:35:50 +0300

    Add 1.3.3. Abbreviation

---
+++ 1.3.3. Abbreviation/src/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	phrase := readString()
+
+	// 1. Разбейте фразу на слова, используя `strings.Fields()`
+	// 2. Возьмите первую букву каждого слова и приведите
+	//    ее к верхнему регистру через `unicode.ToUpper()`
+	// 3. Если слово начинается не с буквы, игнорируйте его
+	//    проверяйте через `unicode.IsLetter()`
+	// 4. Составьте слово из получившихся букв и запишите его
+	//    в переменную `abbr`
+	// ...
+
+	fmt.Println(string(abbr))
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// readString читает строку из `os.Stdin` и возвращает ее
+func readString() string {
+	rdr := bufio.NewReader(os.Stdin)
+	str, _ := rdr.ReadString('\n')
+	return str
+}
