f9450286ba2ef589f581c15f16a7c1f70d75f8c0
Andrey Balamut <balamut.andrey@gmail.com>
2023-01-10 15:56:14 +0300

    Fix 1.3.1. Shortening of a byte line -- code

--- 1.3.1. Shortening of a byte line/src/main.go
+++ 1.3.1. Shortening of a byte line/src/main.go
@@ -2,40 +2,18 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	var number int
-	fmt.Scanf("%d", &number)
-
-	// Посчитайте, сколько раз каждая цифра встречается
-	// в числе `number`. Запишите результат в карту `counter`,
-	// где ключом является цифра числа, а значением -
-	// сколько раз она встречается
-	counter := make(map[int]int)
-	// ...
-
-	printCounter(counter)
-}
-
-// ┌─────────────────────────────────┐
-// │ не меняйте код ниже этой строки │
-// └─────────────────────────────────┘
-
-// printCounter печатает карту в формате
-// key1:val1 key2:val2 ... keyN:valN
-func printCounter(counter map[int]int) {
-	digits := make([]int, 0)
-	for d := range counter {
-		digits = append(digits, d)
+	var text, res string
+	var width int
+	fmt.Scanf("%s %d", &text, &width)
+	if len(text) <= width {
+		res = text
+	} else {
+		str := []byte(text)
+		str = str[:width]
+		res = string(str) + "..."
 	}
-	sort.Ints(digits)
-	for idx, digit := range digits {
-		fmt.Printf("%d:%d", digit, counter[digit])
-		if idx < len(digits)-1 {
-			fmt.Print(" ")
-		}
-	}
-	fmt.Print("\n")
+	fmt.Println(res)
 }
